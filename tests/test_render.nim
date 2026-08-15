b438bf66a614cef36dd6cf3d774cf4250721c7d5
Loïc Bartoletti <lbartoletti@lituus-lab.com>
2026-08-15 14:21:08 +0200

    test(scene): verify contract and runtime guards
    Signed-off-by: Loïc Bartoletti <lbartoletti@lituus-lab.com>

--- tests/test_render.nim
+++ tests/test_render.nim
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2026 lituus-lab
 import std/[unittest, strutils]
+import contracts
 import UniGlyph
 import UniPlot
 
@@ -21,3 +22,19 @@ suite "rendering":
     check png.len > 8
     check png[0 .. 3] == @[137'u8, 80, 78, 71]
 
+  test "scene and render boundaries reject invalid values":
+    let white = defaultTheme().background
+    when defined(release):
+      expect PlotError: discard initScene(Size(width: 0, height: 10), white)
+    else:
+      expect PreConditionDefect:
+        discard initScene(Size(width: 0, height: 10), white)
+    var scene = initScene(Size(width: 10, height: 10), white)
+    when defined(release):
+      expect PlotError:
+        scene.addText("x", Point(x: NaN, y: 1), 12, white)
+    else:
+      expect PreConditionDefect:
+        scene.addText("x", Point(x: NaN, y: 1), 12, white)
+    expect PlotError: discard scene.toSvg(nil)
+    expect PlotError: discard scene.renderImage(nil)
